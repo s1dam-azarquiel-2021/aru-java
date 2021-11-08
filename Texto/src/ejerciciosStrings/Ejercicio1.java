4395b5ce3436e5761165dcef9d283e5d9fa30b28
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-08 19:35:14 +0100

    Adding Ejercicio1 to ejerciciosStrings package

---
+++ Texto/src/ejerciciosStrings/Ejercicio1.java
@@ -0,0 +1,26 @@
+package ejerciciosStrings;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		// Programa que pase un String a mayusculas
+		// y diga cuantas letras han cambiado
+
+		Scanner input = new Scanner(System.in);
+		String frase = input.nextLine();
+		input.close();
+
+		int cambios = 0;
+		for (int i = 0; i < frase.length(); i++) {
+			if (Character.isLowerCase(frase.charAt(i))) {
+				cambios++;
+			}
+		}
+
+		System.out.printf(
+			"Frase en mayusculas: %s\n" +
+			"Numero de letras cambiadas: %d",
+			frase.toUpperCase(), cambios);
+	}
+}
