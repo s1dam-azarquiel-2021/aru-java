486c982673ac75952f61002fa8a3b5f6cc9c0d64
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-09 09:35:16 +0100

    Adding Ejercicio5 to ejerciciosStrings package

---
+++ Texto/src/ejerciciosStrings/Ejercicio5.java
@@ -0,0 +1,25 @@
+package ejerciciosStrings;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		// Programa que lea un numero double, lo convierta a una cadena
+		// y le meta las comas separadoras de los miles
+
+		System.out.print("Introduce un numero: ");
+		Scanner input = new Scanner(System.in);
+		double numero = input.nextDouble();
+		input.close();
+
+		StringBuilder numeroFinal = new StringBuilder(String.valueOf(numero));
+		for (int i = numeroFinal.indexOf(".") - 3; i > 0; i -= 3) {
+			numeroFinal.insert(i, ",");
+		}
+
+		System.out.printf(
+			"Numero con comas: %s",
+			numeroFinal
+		);
+	}
+}
