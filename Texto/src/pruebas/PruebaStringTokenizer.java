765ef2d6fc38851214a4405900f91fbcc5911eb4
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-09 09:02:32 +0100

    Adding PruebaStringTokenizer to pruebas package

---
+++ Texto/src/pruebas/PruebaStringTokenizer.java
@@ -0,0 +1,11 @@
+package pruebas;
+
+import java.util.StringTokenizer;
+
+public class PruebaStringTokenizer {
+	public static void main(String[] args) {
+		String frase = "Buenos dias. A todos.";
+		StringTokenizer st = new StringTokenizer(frase, " ,.");
+		System.out.println(st.countTokens());
+	}
+}
