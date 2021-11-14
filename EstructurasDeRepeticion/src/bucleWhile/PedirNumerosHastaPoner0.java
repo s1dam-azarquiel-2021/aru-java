dd743b878a7d9686eb26acc6c55af52a8128236c
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-14 20:55:45 +0100

    Typo in filename

--- EstructurasDeRepeticion/src/bucleWhile/PedirNumerosHastaPoner0.java
+++ EstructurasDeRepeticion/src/bucleWhile/PedirNumerosHastaPoner0.java
@@ -2,20 +2,20 @@ package bucleWhile;
 
 import java.util.Scanner;
 
-public class PedirNumerosHastPoner0 {
+public class PedirNumerosHastaPoner0 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = -1;
 		while (n != 0) {
-			System.out.print("Introduce un numero: ");
+			System.out.printf("Introduce un numero: ");
 			n = input.nextInt();
 		}
 		input.close();
-		
+
 		Scanner input2 = new Scanner(System.in);
 		int n2;
 		do {
-			System.out.print("Introduce un numero: ");
+			System.out.printf("Introduce un numero: ");
 			n2 = input2.nextInt();
 		} while (n2 != 0);
 		input2.close();
