e9b8f02ad16e935db3d738bad1decde69c37aafa
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-22 13:43:04 +0100

    Adding Temperaturas to pruebas package

---
+++ Arrays/src/pruebas/Temperaturas.java
@@ -0,0 +1,23 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Temperaturas {
+	public static void main(String[] args) {
+		double[] temps = new double[7];
+		Scanner input = new Scanner(System.in);
+		for (int i = 0; i < temps.length; i++) {
+			System.out.printf("Introduce una temperatura: ");
+			temps[i] = input.nextDouble();
+		}
+		input.close();
+
+		double media = 0;
+		for (int i = 0; i < temps.length; i++) {
+			media += temps[i];
+		}
+		media /= temps.length;
+
+		System.out.printf("Temperatura media: %.2f\n", media);
+	}
+}
