5365b571b64d0c50ded09dfa7210cb80d7e1fcc5
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-30 18:32:19 +0100

    Adding more stuff to Notas2DConNombres in pruebas package

--- Arrays/src/pruebas/Notas2DConNombres.java
+++ Arrays/src/pruebas/Notas2DConNombres.java
@@ -36,14 +36,45 @@ public class Notas2DConNombres {
 
 		System.out.printf("\n");
 
-		for (int i = 0; i < notas[0].length; i++) {
-			System.out.printf("%s:", modulos[i]);
-			for (int j = 0; j < notas.length; j++) {
-				if (notas[j][i] >= 5) {
-					System.out.printf(" %s", alumnos[j]);
+		for (int j = 0; j < notas[0].length; j++) {
+			System.out.printf("%s:", modulos[j]);
+			for (int i = 0; i < notas.length; i++) {
+				if (notas[i][j] >= 5) {
+					System.out.printf(" %s", alumnos[i]);
 				}
 			}
 			System.out.printf("\n");
 		}
+
+		System.out.printf("\n");
+
+		for(int i = 0; i < notas.length; i++) {
+			int suma = 0;
+			for (int j = 0; j < notas[i].length; j++) {
+				suma += notas[i][j];
+			}
+			System.out.printf(
+				"%s: %.1f\n",
+				alumnos[i], (double) (suma / notas[i].length));
+		}
+
+		System.out.printf("\n");
+
+		double mejorMedia = 0;
+		int iMejorMedia = 0;
+
+		for (int j = 0; j < notas[0].length; j++) {
+			int suma = 0;
+			for (int i = 0; i < notas.length; i++) {
+				suma += notas[i][j];
+			}
+			double media = suma / notas.length;
+			if (media > mejorMedia)	{
+				mejorMedia = media;
+				iMejorMedia = j;
+			}
+		}
+
+		System.out.printf("%s: %.1f\n", modulos[iMejorMedia], mejorMedia);
 	}
 }
