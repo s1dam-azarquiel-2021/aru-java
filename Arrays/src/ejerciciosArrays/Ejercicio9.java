a569650d98ca8d11265799414c631d8baddb0bfd
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-04 19:17:21 +0100

    Adding Ejercicio9 to ejerciciosArrays package

---
+++ Arrays/src/ejerciciosArrays/Ejercicio9.java
@@ -0,0 +1,45 @@
+package ejerciciosArrays;
+
+public class Ejercicio9 {
+	public static void main(String[] args) {
+		// Programa que mezcla dos arrays ordenados de N elementos, consiguiendo
+		// un array de n*2 elementos igualmente ordenados.
+
+		int[] arrA = { 4, 8, 15, 22, 25, 40 };
+		int[] arrB = { 3, 6, 18, 21, 25, 47 };
+
+		int[] arrFinal = new int[arrA.length + arrB.length];
+
+		int iA = 0;
+		int iB = 0;
+		int iF = 0;
+		while (iA < arrA.length || iB < arrB.length) {
+			if (iA == arrA.length) {
+				arrFinal[iF] = arrB[iB];
+				iB++;
+			} else if (iB == arrB.length) {
+				arrFinal[iF] = arrA[iA];
+				iA++;
+			} else if (arrA[iA] < arrB[iB]) {
+				arrFinal[iF] = arrA[iA];
+				iA++;
+			} else if (arrB[iB] < arrA[iA]) {
+				arrFinal[iF] = arrB[iB];
+				iB++;
+			} else {
+				arrFinal[iF] = arrA[iA];
+				iF++;
+				arrFinal[iF] = arrA[iB];
+				iA++;
+				iB++;
+			}
+
+			iF++;
+		}
+
+		System.out.printf("Array final:\n");
+		for (int n : arrFinal) {
+			System.out.printf("%d ", n);
+		}
+	}
+}
