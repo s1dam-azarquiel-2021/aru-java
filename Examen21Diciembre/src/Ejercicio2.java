e1d9afa5363604f12a330a93a6f123911611eeb1
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-21 19:34:50 +0100

    [fix] I dunno how to read

--- Examen21Diciembre/src/Ejercicio2.java
+++ Examen21Diciembre/src/Ejercicio2.java
@@ -7,16 +7,18 @@ public class Ejercicio2 {
 		int n = input.nextInt();
 		input.close();
 
+		int persistencia = 0;
 		while (n > 9) {
 			int nCopia = n;
 			n = 1;
+			persistencia++;
 
-			for (int i; nCopia > 0; nCopia /= 10) {
-				i = nCopia % 10;
-				n *= i;
+			while (nCopia > 0) {
+				n *= nCopia % 10;
+				nCopia /= 10;
 			}
 		}
 
-		System.out.printf("Persistencia multiplicativa: %d\n", n);
+		System.out.printf("Persistencia multiplicativa: %d\n", persistencia);
 	}
 }
