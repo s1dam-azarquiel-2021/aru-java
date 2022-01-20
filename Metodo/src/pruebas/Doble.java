dbff474df7c4bdd8417249a4ce4c3b56f08c5dca
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-20 10:42:00 +0100

    [add] Doble - pruebas - Metodo

---
+++ Metodo/src/pruebas/Doble.java
@@ -0,0 +1,11 @@
+package pruebas;
+
+public class Doble {
+	public static void main(String[] args) {
+		System.out.printf("Doble de 5: %d\n", doble(5));
+	}
+
+	public static int doble (int n) {
+		return n * 2;
+	}
+}
