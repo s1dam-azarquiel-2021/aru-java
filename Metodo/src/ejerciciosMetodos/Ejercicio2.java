efe22b20ae7860a2769157e4f3726a1f7573308c
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-20 10:44:01 +0100

    [add] Ejercicio2 - ejerciciosMetodos - Metodo

---
+++ Metodo/src/ejerciciosMetodos/Ejercicio2.java
@@ -0,0 +1,11 @@
+package ejerciciosMetodos;
+
+public class Ejercicio2 {
+	public static double grados(int n) {
+		return 9.0 / 5 * n + 32;
+	}
+
+	public static double grados(double n) {
+		return 9.0 / 5 * n + 32;
+	}
+}
