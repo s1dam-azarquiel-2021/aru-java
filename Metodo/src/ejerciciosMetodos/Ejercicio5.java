c1ab646778984ecde86b37576a994a0f474d4f7d
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-23 19:45:40 +0100

    [add] Ejercicio5 - ejerciciosMetodos - Metodo

---
+++ Metodo/src/ejerciciosMetodos/Ejercicio5.java
@@ -0,0 +1,14 @@
+package ejerciciosMetodos;
+
+public class Ejercicio5 {
+	public static int fecha(int d, int m) {
+		final int[] diasMeses = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+		int dias = 0;
+		for (int i = 1; i < m; i++) {
+			dias += diasMeses[i - 1];
+		}
+
+		return dias + d;
+	}
+}
