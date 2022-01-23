b5e3e5faecc1eb66b5e0362a041bf64cec0b46c2
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-23 19:46:17 +0100

    [add] Ejercicio8 - ejerciciosMetodos - Metodo

---
+++ Metodo/src/ejerciciosMetodos/Ejercicio8.java
@@ -0,0 +1,22 @@
+package ejerciciosMetodos;
+
+public class Ejercicio8 {
+	public static String quitaFaltas(String st) {
+		String resultado = "";
+		boolean puntoEncontrado = false;
+		for (int i = 0; i < st.length(); i++) {
+			char c = st.charAt(i);
+			if (c == '.') {
+				puntoEncontrado = true;
+			} else if (
+				puntoEncontrado &&
+				c >= 'a' && c <= 'z'
+			) {
+				c -= 'a' - 'A';
+				puntoEncontrado = false;
+			}
+			resultado += c;
+		}
+		return resultado;
+	}
+}
