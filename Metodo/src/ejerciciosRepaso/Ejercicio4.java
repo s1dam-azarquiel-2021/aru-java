95beac624c924c7ad9c65b4d074b4e2926da8450
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-29 23:08:36 +0100

    [add] Ejercicio4 - ejerciciosRepaso - Metodo

---
+++ Metodo/src/ejerciciosRepaso/Ejercicio4.java
@@ -0,0 +1,30 @@
+package ejerciciosRepaso;
+
+public class Ejercicio4 {
+	public static int[] secuencia(int[] array) {
+		int[] resultado = { 0, 0 };
+		int[] actual = { -1, 1 };
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[i - 1]) {
+				if (actual[0] == -1) {
+					actual[0] = i - 1;
+				}
+				actual[1]++;
+			} else {
+				if (actual[1] > resultado[1]) {
+					resultado[0] = actual[0];
+					resultado[1] = actual[1];
+				}
+				actual[0] = -1;
+				actual[1] = 1;
+			}
+		}
+
+		if (actual[1] > resultado[1]) {
+			resultado[0] = actual[0];
+			resultado[1] = actual[1];
+		}
+
+		return resultado;
+	}
+}
