3ba0b2e33c9ab9064d980eb09ebb49c5d0bb3803
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-29 23:08:47 +0100

    [add] Ejercicio5 - ejerciciosRepaso - Metodo

---
+++ Metodo/src/ejerciciosRepaso/Ejercicio5.java
@@ -0,0 +1,45 @@
+package ejerciciosRepaso;
+
+public class Ejercicio5 {
+	public static int[] ordenarExtravagante(int[] array) {
+		int[] ordenada = array.clone();
+		ordenar(ordenada);
+
+		int[] resultado = new int[ordenada.length];
+		int posPar = 0;
+		int posImpar = resultado.length - 1;
+		for(int i = 0; i < ordenada.length; i++) {
+			if (ordenada[i] % 2 == 0) {
+				resultado[posPar] = ordenada[i];
+				posPar++;
+			} else {
+				resultado[posImpar] = ordenada[i];
+				posImpar--;
+			}
+		}
+
+		return resultado;
+	}
+
+	public static void ordenar(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			intercambiar(array, i, posMenor(array, i));
+		}
+	}
+
+	public static void intercambiar(int[] array, int pos1, int pos2) {
+		int temp = array[pos1];
+		array[pos1] = array[pos2];
+		array[pos2] = temp;
+	}
+
+	public static int posMenor(int[] array, int pos) {
+		for (int i = pos; i < array.length; i++) {
+			if (array[i] < array[pos]) {
+				pos = i;
+			}
+		}
+
+		return pos;
+	}
+}
