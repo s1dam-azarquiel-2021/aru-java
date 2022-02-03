1bf7dad02473370f89f0b05fd81e62448ec9f072
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-02-03 10:52:45 +0100

    [add] Agenda - agenda - Clases

---
+++ Clases/src/agenda/Agenda.java
@@ -0,0 +1,65 @@
+package agenda;
+
+public class Agenda {
+	private Contacto[] lista;
+
+	public Contacto[] getLista() {
+		return lista;
+	}
+
+	public Agenda(int size) {
+		this.lista = new Contacto[size];
+	}
+
+	public boolean add(Contacto contacto) {
+		for (int i = 0; i < size(); i++) {
+			if (this.lista[i] == null) {
+				this.lista[i] = contacto;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean remove(Contacto contacto) {
+		int i = contactIndex(contacto);
+		if (i == -1) {
+			return false;
+		}
+
+		this.lista[i] = null;
+		return true;
+	}
+
+	private int contactIndex(Contacto contacto) {
+		for (int i = 0; i < size(); i++) {
+			if (this.lista[i] != null && this.lista[i].equals(contacto)) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public int size() {
+		return this.lista.length;
+	}
+
+	public String showLista() {
+		String resultado = "";
+		for (int i = 0; i < size(); i++) {
+			if (this.lista[i] != null) {
+				resultado += "Contacto " + (i + 1) + "\n";
+				resultado += this.lista[i].toString();
+			}
+		}
+
+		return resultado;
+	}
+
+	@Override
+	public String toString() {
+		return "Agenda:\n" + showLista();
+	}
+}
