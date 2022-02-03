f4d1e9575e685ce072b9df95d5b45a6387dc84d5
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-02-03 10:50:57 +0100

    [add] CuentaCorrienteProfesional, PruebaProfesional and Cliente - banco - Clases

---
+++ Clases/src/banco/CuentaCorrienteProfesional.java
@@ -0,0 +1,44 @@
+package banco;
+
+public class CuentaCorrienteProfesional {
+	private long numeroCuenta;
+	private Cliente titular;
+	private double saldo;
+
+	public CuentaCorrienteProfesional( long numeroCuenta, Cliente titular) {
+		this.numeroCuenta = numeroCuenta;
+		this.titular = titular;
+		this.saldo = 0;
+	}
+
+	public void ingresar(double saldo) {
+		this.saldo += saldo;
+	}
+
+	public boolean sacar(double saldo) {
+		if (saldo > this.saldo) {
+			return false;
+		}
+
+		this.saldo -= saldo;
+		return true;
+	}
+
+	public long getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	@Override
+	public String toString() {
+		return "Numero cuenta: " + numeroCuenta + "\n\nTitular:\n" + titular
+			+ "\n\nSaldo: " + saldo;
+	}
+}
