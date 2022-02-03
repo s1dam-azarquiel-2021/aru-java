8f4e62e1144e2b526403c27cc3cdf7f56a6cb391
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-02-03 10:52:52 +0100

    [add] Prueba - agenda - Clases

---
+++ Clases/src/agenda/Prueba.java
@@ -0,0 +1,17 @@
+package agenda;
+
+public class Prueba {
+	public static void main(String[] args) {
+		Contacto juan = new Contacto("Juan", 123456789);
+		Contacto fonsi = new Contacto("Fonsi", 234567891);
+		Contacto hugo = new Contacto("Hugo", 345678912);
+		Agenda agenda = new Agenda(20);
+		agenda.add(juan);
+		agenda.add(fonsi);
+		agenda.add(hugo);
+		System.out.printf("%s\n", agenda);
+		agenda.remove(juan);
+		agenda.remove(hugo);
+		System.out.printf("%s\n", agenda);
+	}
+}
