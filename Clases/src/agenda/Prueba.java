e84ea2bcc4696384beebad1d7e517d9885378671
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-02-07 13:42:33 +0100

    [add] More tests - Prueba - agenda - Clases

--- Clases/src/agenda/Prueba.java
+++ Clases/src/agenda/Prueba.java
@@ -10,10 +10,15 @@ public class Prueba {
 		agenda.add(fonsi);
 		agenda.add(hugo);
 		System.out.printf("%s\n", agenda);
+		System.out.printf("%d\n", agenda.saved());
 		agenda.remove(juan);
 		agenda.remove(hugo);
 		System.out.printf("%s\n", agenda);
 		System.out.printf("%s\n", agenda.find("Fonsi"));
+		System.out.printf("%s\n", agenda.find(234567891));
 		System.out.printf("%s\n", agenda.find("Pepino"));
+		System.out.printf("%s\n", agenda.find(456789123));
+		agenda.remove("Fonsi");
+		System.out.printf("%s\n", agenda);
 	}
 }
