2f15edf94d565d41c2fb27658ee1ebda735de9b6
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-03-31 09:44:16 +0200

    [add] Colores - dialogos - Ventanas

---
+++ Ventanas/src/dialogos/Colores.java
@@ -0,0 +1,67 @@
+package dialogos;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+public class Colores extends JFrame {
+	private JLabel lblTexto;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(
+						UIManager.getSystemLookAndFeelClassName()
+					);
+					Colores frame = new Colores();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Colores() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 250, 160);
+
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(null);
+		this.setContentPane(contentPane);
+
+		lblTexto = new JLabel("Texto de Prueba");
+		lblTexto.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTexto.setFont(new Font("Iosevka", Font.BOLD, 20));
+		lblTexto.setBounds(20, 20, 200, 60);
+		contentPane.add(lblTexto);
+
+		JButton btnCambiarColor = new JButton("Cambiar color");
+		btnCambiarColor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialogoCambiarColor();
+			}
+		});
+		btnCambiarColor.setBounds(20, 80, 200, 30);
+		contentPane.add(btnCambiarColor);
+	}
+
+	private void dialogoCambiarColor() {
+		DialogoColores dialogoColores = new DialogoColores(this, true);
+		dialogoColores.setLocationRelativeTo(this);
+		dialogoColores.setVisible(true);
+	}
+
+	public JLabel getLblTexto() {
+		return this.lblTexto;
+	}
+}
---
+++ Ventanas/src/dialogos/DialogoColores.java
@@ -0,0 +1,133 @@
+package dialogos;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class DialogoColores extends JDialog {
+	private final ButtonGroup grupoColores = new ButtonGroup();
+	private JRadioButton rdbtnNegro;
+	private JRadioButton rdbtnRojo;
+	private JRadioButton rdbtnAzul;
+	private JRadioButton rdbtnVerde;
+	private JRadioButton rdbtnAmarillo;
+
+	public DialogoColores(JFrame padre, boolean modal) {
+		super(padre, modal);
+		this.setBounds(0, 0, 240, 260);
+		this.getContentPane().setLayout(new BorderLayout());
+
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(null);
+		this.getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		rdbtnNegro = new JRadioButton("Negro");
+		rdbtnNegro.setBounds(20, 20, 200, 30);
+		rdbtnNegro.setActionCommand("negro");
+		contentPanel.add(rdbtnNegro);
+		this.grupoColores.add(rdbtnNegro);
+
+		rdbtnRojo = new JRadioButton("Rojo");
+		rdbtnRojo.setBounds(20, 60, 200, 30);
+		rdbtnRojo.setActionCommand("rojo");
+		contentPanel.add(rdbtnRojo);
+		this.grupoColores.add(rdbtnRojo);
+
+		rdbtnAzul = new JRadioButton("Azul");
+		rdbtnAzul.setBounds(20, 100, 200, 30);
+		rdbtnAzul.setActionCommand("azul");
+		contentPanel.add(rdbtnAzul);
+		this.grupoColores.add(rdbtnAzul);
+
+		rdbtnVerde = new JRadioButton("Verde");
+		rdbtnVerde.setBounds(20, 140, 200, 30);
+		rdbtnVerde.setActionCommand("verde");
+		contentPanel.add(rdbtnVerde);
+		this.grupoColores.add(rdbtnVerde);
+
+		rdbtnAmarillo = new JRadioButton("Amarillo");
+		rdbtnAmarillo.setBounds(20, 180, 200, 30);
+		rdbtnAmarillo.setActionCommand("amarillo");
+		contentPanel.add(rdbtnAmarillo);
+		this.grupoColores.add(rdbtnAmarillo);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		this.getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				confirmar();
+			}
+		});
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		this.getRootPane().setDefaultButton(okButton);
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cancelar();
+			}
+		});
+		cancelButton.setActionCommand("Cancel");
+		buttonPane.add(cancelButton);
+
+		activarBotonAdecuado();
+	}
+
+	private void activarBotonAdecuado() {
+		final Color foreground =
+			((Colores) this.getParent()).getLblTexto().getForeground();
+
+		if (foreground.equals(Color.RED)) {
+			this.rdbtnRojo.setSelected(true);
+		} else if (foreground.equals(Color.BLUE)) {
+			this.rdbtnAzul.setSelected(true);
+		} else if (foreground.equals(Color.GREEN)) {
+			this.rdbtnVerde.setSelected(true);
+		} else if (foreground.equals(Color.YELLOW)) {
+			this.rdbtnAmarillo.setSelected(true);
+		} else {
+			this.rdbtnNegro.setSelected(true);
+		}
+	}
+
+	private void cancelar() {
+		this.dispose();
+	}
+
+	private void confirmar() {
+		Color color;
+		switch (this.grupoColores.getSelection().getActionCommand()) {
+		case "rojo":
+			color = Color.RED;
+			break;
+		case "azul":
+			color = Color.BLUE;
+			break;
+		case "verde":
+			color = Color.GREEN;
+			break;
+		case "amarillo":
+			color = Color.YELLOW;
+			break;
+		case "negro":
+		default:
+			color = Color.BLACK;
+		}
+
+		((Colores) this.getParent()).getLblTexto().setForeground(color);
+		this.dispose();
+	}
+}
