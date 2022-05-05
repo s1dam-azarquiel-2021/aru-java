37a58ca7b538677bd245f1b144004288631face4
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-05 09:25:13 +0200

    [add] blockDeNotas - Archivos

---
+++ Archivos/src/blocDeNotas/GestorArchivoTexto.java
@@ -0,0 +1,55 @@
+package blocDeNotas;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import customReaderWriter.CustomBufferedReader;
+import customReaderWriter.CustomBufferedWriter;
+
+public class GestorArchivoTexto {
+	private String archivo;
+	private String contenido;
+
+	public GestorArchivoTexto(String archivo) {
+		super();
+		this.archivo = archivo;
+	}
+
+	public void readContenido() {
+		CustomBufferedReader br = new CustomBufferedReader() {
+			public void main() throws IOException {
+				contenido = stream.lines().collect(
+					Collectors.joining(System.lineSeparator())
+				);
+			}
+		};
+
+		br.read(archivo);
+	}
+
+	public void guardarContenido() {
+		CustomBufferedWriter bw = new CustomBufferedWriter() {
+			public void main() throws IOException {
+				stream.write(contenido);
+			}
+		};
+
+		bw.write(archivo);
+	}
+
+	public String getArchivo() {
+		return archivo;
+	}
+
+	public void setArchivo(String archivo) {
+		this.archivo = archivo;
+	}
+
+	public String getContenido() {
+		return this.contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+}
---
+++ Archivos/src/blocDeNotas/VentanaPrincipal.java
@@ -0,0 +1,134 @@
+package blocDeNotas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+
+public class VentanaPrincipal extends JFrame {
+	private GestorArchivoTexto gestor = new GestorArchivoTexto("");
+	private JTextArea textArea;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(
+						UIManager.getSystemLookAndFeelClassName()
+					);
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public VentanaPrincipal() {
+		this.setTitle("Bloc de notas");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 531, 407);
+
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BorderLayout(0, 0));
+		this.setContentPane(contentPane);
+
+		this.textArea = new JTextArea();
+
+		JMenuBar menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+		JMenuItem mntmGuardarComo = new JMenuItem("Guardar como");
+
+		mnArchivo.add(mntmAbrir);
+		mnArchivo.add(mntmGuardar);
+		mnArchivo.add(mntmGuardarComo);
+
+		JMenu mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(textArea);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+
+		mntmAbrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				abrir();
+			}
+		});
+
+		mntmAbrir.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK)
+		);
+
+		mntmGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guardar();
+			}
+		});
+
+		mntmGuardar.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK)
+		);
+
+		mntmGuardarComo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guardarComo();
+			}
+		});
+	}
+
+	private void abrir() {
+		JFileChooser dialogo = new JFileChooser(this.gestor.getArchivo());
+		dialogo.showOpenDialog(this);
+
+		if (dialogo.getSelectedFile() != null) {
+			this.gestor.setArchivo(dialogo.getSelectedFile().getAbsolutePath());
+			this.gestor.readContenido();
+			this.textArea.setText(this.gestor.getContenido());
+
+			this.setTitle(this.gestor.getArchivo() + ":Bloc de notas");
+		}
+	}
+
+	private void guardarComo() {
+		JFileChooser dialogo = new JFileChooser(gestor.getArchivo());
+		dialogo.showSaveDialog(this);
+
+		if (dialogo.getSelectedFile() != null) {
+			this.gestor.setArchivo(dialogo.getSelectedFile().getAbsolutePath());
+			this.gestor.setContenido(this.textArea.getText());
+			this.gestor.guardarContenido();
+
+			this.setTitle(this.gestor.getArchivo() + ":Bloc de notas");
+		}
+	}
+
+	private void guardar() {
+		if (this.gestor.getArchivo() != null) {
+			this.gestor.setContenido(this.textArea.getText());
+			this.gestor.guardarContenido();
+		} else {
+			this.guardarComo();
+		}
+	}
+}
